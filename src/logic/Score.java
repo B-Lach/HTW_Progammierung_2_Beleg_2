19e09f174cd941d3cc5f1a7f630d598d2f49473f
Benny Lach <benny@discollect.me>
2016-11-28 11:18:07 +0100

    Added Score class

---
+++ src/logic/Score.java
@@ -0,0 +1,29 @@
+package logic;
+
+/**
+ * Because Java has no implementation of tuples we have to build a custom class to hold the current score for the player
+ * @author Benny Lach
+ *
+ */
+public class Score {
+	private int p1;
+	private int p2;
+	
+	public Score(int p1, int p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+
+	public int getP1() {
+		return p1;
+	}
+
+	public int getP2() {
+		return p2;
+	}
+	
+	@Override
+	public String toString() {
+		return "Score for Player 1: " + p1 + " - Score for Player 2: " + p2;
+	}
+}
