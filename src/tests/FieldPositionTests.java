ed0172d8b4b955b8d28fae54cff40be822633154
Benny Lach <benny@discollect.me>
2016-12-01 19:31:51 +0100

    improved FieldPosition tests

--- src/tests/FieldPositionTests.java
+++ src/tests/FieldPositionTests.java
@@ -16,19 +16,16 @@ public class FieldPositionTests {
 	}
 	
 	@Test
-	public void test_equalityTrue() {
+	public void test_equality() {
 		FieldPosition p = new FieldPosition(1, 3);
 		FieldPosition p2 = new FieldPosition(1, 3);
+		FieldPosition p3 = new FieldPosition(1,1);
 		
-		assertEquals("Positions are not equal", p, p2);
-	}
-	
-	@Test
-	public void test_equalityFalse() {
-		FieldPosition p = new FieldPosition(1, 3);
-		FieldPosition p2 = new FieldPosition(2, 3);
-		
-		assertNotEquals("Positions are not equal", p, p2);
+		assertEquals("Equality Check with self is not true", true, p.equals(p));
+		assertEquals("Equality Check with null is not false", false, p.equals(null));
+		assertEquals("Equality Check with another class is not false", false, p.equals(3));
+		assertEquals("Equality Check with same values is not true", true, p.equals(p2));
+		assertEquals("Equality Check with different values is not false", false, p.equals(p3));
 	}
 
 }
